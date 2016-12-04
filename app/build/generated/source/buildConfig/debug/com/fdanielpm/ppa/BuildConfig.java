4a90ea93bc6dba58e1c87ec577aa9a5866101e42
darkp <fdanielpm@yandex.com>
2016-12-03 20:47:43 -0600

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/fdanielpm/ppa/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fdanielpm.ppa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fdanielpm.ppa";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
